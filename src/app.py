18ee46ee82f8ea827753be70545102d6f0f03ab2
alex-billybob <alex.yishun@gmail.com>
2025-08-31 00:44:30 +0800

    finishing touches

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Test App with Sidebar")
+st.title("MLOPS Integrated App")
 
-st.write("This is the main content of the app.")
+st.write("Predictions for used car prices in India (Alex) and Classification of Wheat Seeds (Thadenn)")
